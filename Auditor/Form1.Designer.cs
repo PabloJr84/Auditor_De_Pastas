0193469e18142b93800619e8ca4903ea6b5029c7
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-27 00:31:16 -0300

    Primeiro Commit

---
+++ Auditor/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Auditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Btn_Verificador = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Carregar Arquivos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Carregar Imagens";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 498);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(466, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(609, 498);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(221, 109);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(221, 498);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Diretório";
+            // 
+            // Btn_Verificador
+            // 
+            this.Btn_Verificador.Location = new System.Drawing.Point(636, 32);
+            this.Btn_Verificador.Name = "Btn_Verificador";
+            this.Btn_Verificador.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Verificador.TabIndex = 6;
+            this.Btn_Verificador.Text = "Verificador";
+            this.Btn_Verificador.UseVisualStyleBackColor = true;
+            this.Btn_Verificador.Click += new System.EventHandler(this.Btn_Verificador_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 634);
+            this.Controls.Add(this.Btn_Verificador);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Auditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button Btn_Verificador;
+    }
+}
+
---
+++ Auditor/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; // Manipulação e visualisação de arquivos
+
+namespace Auditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(listBox1.SelectedItem.ToString());
+            }
+            catch (Exception)
+            { }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            
+            label1.Text = folderBrowserDialog1.SelectedPath;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            String[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+            listBox1.Items.AddRange(files);
+
+            DirectoryInfo dir_files = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            FileInfo[] files2 = dir_files.GetFiles("*", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2)
+            {
+                listBox2.Items.Add(fil.Name);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+
+            label1.Text = folderBrowserDialog1.SelectedPath;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            String[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.bmp");
+            listBox1.Items.AddRange(files);
+            files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg");
+            listBox1.Items.AddRange(files);
+            files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.png");
+            listBox1.Items.AddRange(files);
+
+            DirectoryInfo dir_files = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            FileInfo[] files2 = dir_files.GetFiles("*.bmp", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2){ listBox2.Items.Add(fil.Name);}
+            files2 = dir_files.GetFiles("*.png", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2) { listBox2.Items.Add(fil.Name); }
+            files2 = dir_files.GetFiles("*.jpg", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2) { listBox2.Items.Add(fil.Name); 
+    
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(folderBrowserDialog1.SelectedPath + "\\" + listBox2.SelectedItem.ToString());
+            }
+            catch (Exception)
+            { }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] diretorios = Directory.GetDirectories( @"Y:\CFTV\AM_Contagem\2022\08-Agosto\14");
+
+            foreach (var diretori in diretorios) 
+            { 
+            listBox1.Items.Add("*"+ diretorios);
+            }
+        }
+
+        private string[] Getfiles(string path)
+        {
+            return Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+        }
+
+        private void Btn_Verificador_Click(object sender, EventArgs e)
+        {
+            //Verificador de pastas existente
+            if (Directory.Exists(@"D:\Users\50036490\OneDrive - ArcelorMittal\Desktop\BRM-PABLO\154693"))
+                MessageBox.Show("Pasta existente");
+            else
+                MessageBox.Show("Nãso existe");
+
+        }
+    }
+}
